09c943d0377585f549cf6858e25cfb5a93b9bbc7
8-prime <hoffmeister.stefan99@gmail.com>
2026-04-25 00:16:33 +0200

    clean slate for rust

--- src/types/archive.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-
-pub const UnPuffError = error{InvalidArchiveHeader};
-pub const ArchiveType = enum { plain };
-pub const Puff = struct { files: std.ArrayList(PuffEntry), type: i64 };
-pub const PuffEntry = struct { relativePath: []const u8, puffedLength: u64, tempStartOffset: u64 };
